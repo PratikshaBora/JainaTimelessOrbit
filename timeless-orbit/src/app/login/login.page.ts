f8f7e0384beb822b42eeb215f62fb2902c410c25
PratikshaBora <pratu0225@gmail.com>
2025-12-06 20:54:17 +0530

    Added polished login and home page UI with CSS

--- timeless-orbit/src/app/login/login.page.ts
+++ timeless-orbit/src/app/login/login.page.ts
@@ -1,3 +1,4 @@
+import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
@@ -13,19 +14,22 @@ export class LoginPage implements OnInit {
   password: string='';
   loginForm!: FormGroup;
 
-  constructor(private fb: FormBuilder) { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router
+  ) {}
 
   ngOnInit() {
-    this.loginForm = this.fb.group({
-      email: ['', [Validators.required, Validators.email]],
+    this.loginForm = this.formBuilder.group({
+      username: ['', [Validators.required, Validators.minLength(3)]],
       password: ['', [Validators.required, Validators.minLength(6)]],
     });
   }
 
   onLogin() {
     if (this.loginForm.valid) {
-      const { email, password } = this.loginForm.value;
-      console.log('Login attempt:', email, password);
+      const { username, password } = this.loginForm.value;
+      console.log('Login attempt:', username, password);
       // TODO: Hook into authentication service
     }
   }
@@ -33,6 +37,7 @@ export class LoginPage implements OnInit {
   checkLogin() {
     if (this.username === 'pratiksha' && this.password === '1234') {
       alert('Login successful! 🎉');
+      this.router.navigate(['/home']);
     } else {
       alert('Invalid username or password ❌');
     }
