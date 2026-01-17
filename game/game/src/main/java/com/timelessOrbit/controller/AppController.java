54f141d0557ac9bd4ee975827929046a40753392
PratikshaBora <pratu0225@gmail.com>
2026-01-17 07:56:29 +0530

    adding routes in controller

--- game/game/src/main/java/com/timelessOrbit/controller/AppController.java
+++ game/game/src/main/java/com/timelessOrbit/controller/AppController.java
@@ -6,7 +6,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class AppController {
 
-	@RequestMapping("/")
+	@RequestMapping({
+		 "/", "/login", "/home", "/lobby", "/room/**", "/winner"
+	})
 	public String index()
 	{
 		return "index";
